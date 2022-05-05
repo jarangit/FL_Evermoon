cb0cee27ab14e8f918320779a023eab9db003199
jarangit <jaran.dch@gmail.com>
2022-05-05 23:44:55 +0700

    first

---
+++ .next/static/Oz4f1SHynOmll4aUtIDBS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/352-72f3be415fd6c552.js","static/chunks/pages/index-10a83a3dbe82872e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
