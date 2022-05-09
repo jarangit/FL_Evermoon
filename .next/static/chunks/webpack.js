d325d57ee0c0cdbbb4b3811904958c69ceec0b4e
jarangit <jaran.dch@gmail.com>
2022-05-10 00:30:00 +0700

    fix: imgv6

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c2af3694a6cfa5c5"; }
+/******/ 		__webpack_require__.h = function() { return "7923f64dd147d08c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
