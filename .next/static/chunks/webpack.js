26b94baf2054bec4418d47c0572751cbaf7f5fa9
jarangit <jaran.dch@gmail.com>
2022-05-12 11:17:43 +0700

    animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "441ea0920f0e49c4"; }
+/******/ 		__webpack_require__.h = function() { return "a368384a8f994e61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/capsules/modal/layoutModal.tsx
+++ components/capsules/modal/layoutModal.tsx
@@ -19,7 +19,7 @@ const LayoutModal = ({ children }: Props) => {
       className={`flex justify-content items-center bg-black/40 backdrop-blur-xl  overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none   transition-opacity  duration-200 opacity-${opct}`}
     >
       <div className={`m-auto max-w-[1120px] lg:w-auto w-[90%] `} >
-        <div className="w-full inline-block lg:w-auto p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-[#1A2035] border-[1px] border-[#5F5F5F] shadow-xl rounded-lg font-source">
+        <div className="w-full inline-block lg:w-auto p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-[#1A2035]/95 border-[1px] border-[#5F5F5F] shadow-xl rounded-lg font-source">
           {children}
         </div>
       </div>
